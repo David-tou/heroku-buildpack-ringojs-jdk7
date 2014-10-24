ebbd00de4c4dc223d28e6f1b999d2eaa8f155831
David-tou <taotao1983217@gmail.com>
2014-10-24 13:09:05 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+// Minimal hello-world HTTP server demo
+
+exports.app = function(req) {
+    return {
+        status: 200,
+        headers: {"Content-Type": "text/plain"},
+        body: ["Hello Ringojs!"]
+    };
+};
+
+if (require.main == module) {
+    require("ringo/httpserver").main(module.id);
+}
+
